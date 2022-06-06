6d2b079cb2a3a8d8608d5c6499aac28c905418d3
“Craig <97258442+craigbucher@users.noreply.github.com>
2022-06-06 13:36:21 -0500

    working python

--- python/stock_picker.py
+++ python/stock_picker.py
@@ -1,2 +1,11 @@
 def picker(prices):
-    pass
+    max_profit = 0;	
+    max_profit_indices = []
+    for i in range(len(prices)):
+        for j in range(i + 1, len(prices)):
+            if (prices[j] - prices[i]) > max_profit:
+                max_profit = (prices[j] - prices[i])
+                max_profit_indices = [i, j]
+    return(max_profit_indices)
+
+#print(picker([17,3,6,9,15,8,6,1,10]))
