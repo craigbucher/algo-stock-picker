71228f71eeb0c574e634985d0928360f15a2aff4
“Craig <97258442+craigbucher@users.noreply.github.com>
2022-06-06 13:18:22 -0500

    working js

--- js/stockPicker.js
+++ js/stockPicker.js
@@ -1,3 +1,14 @@
 exports.picker = function(prices) {
-
+    let maxProfit = 0;	
+    let maxProfitIndices = [];	
+    for (let i = 0; i < prices.length; i++){	
+        for (let j = i + 1; j < prices.length; j++){	
+            if ((prices[j] - prices[i]) > maxProfit){	
+                maxProfit = prices[j] - prices[i];	
+                 maxProfitIndices = [i, j];	
+            }	
+        }	
+    }	
+    return maxProfitIndices;	
 }
+// console.log(exports.picker([17,3,6,9,15,8,6,1,10]));
